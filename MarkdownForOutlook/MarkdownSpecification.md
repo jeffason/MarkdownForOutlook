acd45bfdc3d589b44229c9fe4bdfeea555e820ec
Jeff Smith <jeff.smith@logos.com>
2014-03-12 19:00:52 -0700

    Initial commit of basic AddIn

---
+++ MarkdownForOutlook/MarkdownSpecification.md
@@ -0,0 +1,121 @@
+# Some H1
+Some text
+
+## Some H2
+ * List item 1
+ * List item 2
+ * List item 3 with code that needs escpaing: Class\<TType\>()
+
+_Italic text_ [Link to google](http://google.com/)
+
+```csharp
+public class ExplicitCSharpBlock
+{
+    public int Value { get; set; }
+}
+```
+
+``` csharp
+public class ExplicitCSharpBlockWithSpace
+{
+    public int Value { get; set; }
+}
+```
+
+```c#
+public class ExplicitCSharpBlock
+{
+    public int Value { get; set; }
+}
+```
+
+``` c#
+public class ExplicitCSharpBlockWithSpace
+{
+    public int Value { get; set; }
+}
+```
+
+```csharp
+public class ExplicitCSharpBlockWithLineBreaks
+{
+    public int Value { get; set; }
+	
+	public string Foo { get; set; }
+}
+
+var i = new ExplicitCSharpBlockWithLineBreaks { Value = 42, Foo = "bar" };
+```
+
+```
+public class ImplicitCSharpBlock
+{
+    public int Value { get; set; }
+}
+```
+
+```javascript
+function explicitJs(){
+    alert('foo');
+}
+```
+
+``` javascript
+function explicitJsWithSpace(){
+    alert('foo');
+}
+```
+
+```js
+function explicitJs(){
+    alert('foo');
+}
+```
+
+``` js
+function explicitJsWithSpace(){
+    alert('foo');
+}
+```
+
+```
+function implicitJs(){
+    alert('foo');
+}
+```
+
+```html
+<div>Some explicit html</div>
+```
+
+``` html
+<div>Some explicit html with space</div>
+```
+
+```
+<div>Some implicit html</div>
+```
+
+```css
+#explicitCss { visibility: hidden; }
+```
+
+```css
+#explicitCssWithSpace { visibility: hidden; }
+```
+
+```css
+#implicitCss { visibility: hidden; }
+```
+
+```xml
+<xml><node>Some explicit xml</node></xml>
+```
+
+``` xml
+<xml><node>Some explicit xml with space</node></xml>
+```
+
+```
+<xml><node>Some implicit xml</node></xml>
+```
---
+++ MarkdownForOutlook/ThisAddIn.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Kiwi.Markdown;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace MarkdownForOutlook
+{
+    public partial class ThisAddIn
+    {
+	    private Dictionary<Outlook.MailItem, string> m_markdownUndo = new Dictionary<Outlook.MailItem, string>();
+		private KeyboardHookListener m_keyboardListener;
+		private readonly MarkdownService m_markdownProvider = new MarkdownService(null);
+ 
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+	        m_keyboardListener = new KeyboardHookListener(new AppHooker()) { Enabled = true };
+	        m_keyboardListener.KeyDown += KeyboardListener_KeyDown;
+        }
+
+	    private void KeyboardListener_KeyDown(object sender, KeyEventArgs keyEventArgs)
+	    {
+		    if (keyEventArgs.Alt && keyEventArgs.Control && keyEventArgs.KeyCode == Keys.M)
+		    {
+				var mailItem = Application.ActiveInspector().CurrentItem as Outlook.MailItem;
+			    if (mailItem != null)
+			    {
+				    if (!m_markdownUndo.ContainsKey(mailItem))
+					    m_markdownUndo.Add(mailItem,null);
+
+					// TODO: 
+					// * recognize and show warning whenever undo text will clobber changes made by the user
+					// * don't transform user signature and previous messages in the thread
+					// * cleanup old mail items (will hooking into send work?)
+				    string bodyTransformed;
+				    if (m_markdownUndo[mailItem] != null)
+				    {
+					    bodyTransformed = m_markdownUndo[mailItem];
+					    m_markdownUndo[mailItem] = null;
+				    }
+				    else
+				    {
+					    bodyTransformed = m_markdownProvider.ToHtml(mailItem.Body);
+					    m_markdownUndo[mailItem] = mailItem.HTMLBody;
+				    }
+				    
+					mailItem.HTMLBody = bodyTransformed;
+					keyEventArgs.Handled = true;   
+			    }
+		    }
+	    }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+	        m_keyboardListener.Dispose();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
